282927ff63fc28155605dc0df0c2b3db21679733
lvpeng <731010527@qq.com>
2020-02-22 22:37:13 +0800

    'feat:添加Table组件和分页器'

--- src/views/Article/index.js
+++ src/views/Article/index.js
@@ -1,6 +1,56 @@
 import React, { Component } from 'react'
-import { Card ,Button} from "antd"
-
+import { Card ,Button,Table, Divider, Tag} from "antd"
+const dataSource = [
+    {
+      key: '1',
+      name: '胡彦斌',
+      age: 32,
+      address: '西湖区湖底公园1号'
+    },
+    {
+      key: '2',
+      name: '胡彦祖',
+      age: 42,
+      address: '西湖区湖底公园1号'
+    },
+    {
+      key: '2',
+      name: '胡彦',
+      age: 46,
+      address: '西湖1号'
+    },
+  ];
+  
+  const columns = [
+    {
+      title: '姓名',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: '年龄',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: '住址',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+        title: '操作',
+        dataIndex: 'action',
+        key: 'action',
+        render:(text, record, index)=>{
+          return (
+              <Button>操作</Button>
+          )
+        }
+    },
+  ];
+  
+  const data = [
+  ];
 export default class Article extends Component {
     toExcel(){
         alert("导出excel")
@@ -8,9 +58,9 @@ export default class Article extends Component {
     render() {
         return (
         <Card title="文章列表" extra={<Button onClick={this.toExcel}>导出excel</Button>} style={{ height:'100%' }}>
-            <p>Card content</p>
-            <p>Card content</p>
-            <p>Card content</p>
+            <Table columns={columns} dataSource={dataSource}
+            // 可在Table中使用pagination配置分页器
+            pagination={{pageSize:2,total:100}} />
         </Card>
         )
     }
