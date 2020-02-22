68488f9a7b9a87903184028161c9fb41719c753e
lvpeng <731010527@qq.com>
2020-02-22 22:50:36 +0800

    'feat:添加国际化'

--- src/index.js
+++ src/index.js
@@ -4,23 +4,26 @@ import { HashRouter as Router , Route, Switch, Redirect } from 'react-router-dom
 import { mainRoutes } from './routes'
 import App from './App'
 import './index.less'
-
+import { LocaleProvider } from 'antd';
+import zhCN from 'antd/lib/locale-provider/zh_CN';
 render(
-    <Router>
-        <Switch>
-            <Route path="/admin" render={(routerProps)=>{
-                {/* 需要登录才能访问admin ,所以用render*/}
-                return <App {...routerProps}/>
-            }} />
-            
-            {
-                mainRoutes.map(route => {
-                return <Route key={route.pathname} path={route.pathname} component={route.component} />
-                })
-            }
-            <Redirect to="/admin" from="/" exact />
-            <Redirect to="/404" />
-        </Switch>
-    </Router>,
+    <LocaleProvider locale={zhCN}>
+        <Router>
+            <Switch>
+                <Route path="/admin" render={(routerProps)=>{
+                    {/* 需要登录才能访问admin ,所以用render*/}
+                    return <App {...routerProps}/>
+                }} />
+                
+                {
+                    mainRoutes.map(route => {
+                    return <Route key={route.pathname} path={route.pathname} component={route.component} />
+                    })
+                }
+                <Redirect to="/admin" from="/" exact />
+                <Redirect to="/404" />
+            </Switch>
+        </Router>
+    </LocaleProvider>,
     document.querySelector('#root')
 )
