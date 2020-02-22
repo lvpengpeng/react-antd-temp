a228498b75c003745f2838516438ef9309988712
lvpeng <731010527@qq.com>
2020-02-22 21:54:37 +0800

    feat:添加Card区域组件

--- src/views/Article/index.js
+++ src/views/Article/index.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import { Card ,Button} from "antd"
 
 export default class Article extends Component {
+    toExcel(){
+        alert("导出excel")
+    }
     render() {
         return (
-            <div>
-                文章
-            </div>
+        <Card title="文章列表" extra={<Button onClick={this.toExcel}>导出excel</Button>} style={{ height:'100%' }}>
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
+        </Card>
         )
     }
 }
