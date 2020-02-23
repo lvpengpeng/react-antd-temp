64d690488a8c9db775c14cd31b92e18bcbc19f3a
lvpeng <731010527@qq.com>
2020-02-23 17:45:09 +0800

    'feat:表格内渲染组件'

--- src/views/Article/index.js
+++ src/views/Article/index.js
@@ -1,71 +1,63 @@
 import React, { Component } from 'react'
-import { Card ,Button,Table, Divider, Tag} from "antd"
+import { Card ,Button,Table, Divider,Tooltip, Tag} from "antd"
 import { getArticles } from '../../requests'
+const titleMap = {
+    id:"id",
+    title:"标题",
+    author:"作者",
+    amount:"阅读量",
+    createAt:"创建时间"
+}
 export default class Article extends Component {
   constructor(){
     super()
     this.state={
       offset: 0,
       limited: 10,
-      dataSource : [
-        {
-          key: '1',
-          name: '胡彦斌',
-          age: 32,
-          address: '西湖区湖底公园1号'
-        },
-        {
-          key: '2',
-          name: '胡彦祖',
-          age: 42,
-          address: '西湖区湖底公园1号'
-        },
-        {
-          key: '2',
-          name: '胡彦',
-          age: 46,
-          address: '西湖1号'
-        },
-      ],
-      columns :[
-        {
-          title: '姓名',
-          dataIndex: 'name',
-          key: 'name',
-        },
-        {
-          title: '年龄',
-          dataIndex: 'age',
-          key: 'age',
-        },
-        {
-          title: '住址',
-          dataIndex: 'address',
-          key: 'address',
-        },
-        {
-            title: '操作',
-            dataIndex: 'action',
-            key: 'action',
-            render:(text, record, index)=>{
-              return (
-                  <Button>操作</Button>
-              )
-            }
-        },
-      ]
+      dataSource : [],
+      columns :[]
     }
   }
   componentDidMount(){
     this.getData()
   }
+  getcolumns(obj){
+    const columnKeys = Object.keys(obj)
+    const columns = columnKeys.map(item =>{
+      if(item === 'amount') {
+        return {
+          title: titleMap[item],
+          key: item,
+          render: (text, record) => {
+            const { amount } = record;
+            return (
+              <Tooltip title={amount > 230 ? '超过230' : '没超过230'}>
+                <Tag color={amount > 230 ? 'red' : 'green'}>{record.amount}</Tag>
+              </Tooltip>
+            )
+          }
+        }
+      }
+     return {
+        title: titleMap[item],
+        dataIndex: item,
+        key: item
+      }
+    })
+    return columns
+  }
   getData(){
     getArticles(this.state.offset,this.state.limited).then((res)=>{
       console.log(res.list);
-        // this.setState({
-        //   dataSource:res.list,
-        //   total:res.total
-        // })
+
+      const columns = this.getcolumns(res.list[0]);
+      
+      console.log(columns,'columns');
+        this.setState({
+          dataSource:res.list,
+          total:res.total,
+          columns
+        })
     })
   }
     toExcel(){
@@ -76,7 +68,7 @@ export default class Article extends Component {
         <Card title="文章列表" extra={<Button onClick={this.toExcel}>导出excel</Button>} style={{ height:'100%' }}>
             <Table columns={this.state.columns} dataSource={this.state.dataSource}
             // 可在Table中使用pagination配置分页器
-            pagination={{pageSize:2,total:100}} />
+            pagination={{pageSize:6,total:100}} />
         </Card>
         )
     }
