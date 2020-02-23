b38b873ccd8aea6feb5588d54779f6a4f58726ca
lvpeng <731010527@qq.com>
2020-02-23 14:37:06 +0800

    'feat:request请求封装'

---
+++ src/requests/index.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+import { message } from 'antd'
+const isDev = process.env.NODE_ENV === 'development'
+
+const service = axios.create({
+  baseURL: isDev ? 'http://rap2api.taobao.org/app/mock/176929' : ''
+})
+
+const service1 = axios.create({
+  baseURL: isDev ? 'http://rap2api.taobao.org/app/mock/176929' : ''
+})
+
+service.interceptors.request.use((config) => {
+  config.data = Object.assign({}, config.data, {
+    // authToken: window.localStorage.getItem('authToken')
+    authToken: 'itisatokenplaceholder'
+  })
+  return config
+})
+
+service.interceptors.response.use((resp) => {
+  if (resp.data.code === 200) {
+    return resp.data.data
+  } else {
+    // 全局处理错误
+    message.error(resp.data.errMsg)
+  }
+})
+
+
+// 获取文章列表
+export const getArticles = (offset = 0, limited = 10) => {
+  return service.post('/api/v1/articleList', {
+    offset,
+    limited
+  })
+}
--- src/views/Article/index.js
+++ src/views/Article/index.js
@@ -1,64 +1,80 @@
 import React, { Component } from 'react'
 import { Card ,Button,Table, Divider, Tag} from "antd"
-const dataSource = [
-    {
-      key: '1',
-      name: '胡彦斌',
-      age: 32,
-      address: '西湖区湖底公园1号'
-    },
-    {
-      key: '2',
-      name: '胡彦祖',
-      age: 42,
-      address: '西湖区湖底公园1号'
-    },
-    {
-      key: '2',
-      name: '胡彦',
-      age: 46,
-      address: '西湖1号'
-    },
-  ];
-  
-  const columns = [
-    {
-      title: '姓名',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: '年龄',
-      dataIndex: 'age',
-      key: 'age',
-    },
-    {
-      title: '住址',
-      dataIndex: 'address',
-      key: 'address',
-    },
-    {
-        title: '操作',
-        dataIndex: 'action',
-        key: 'action',
-        render:(text, record, index)=>{
-          return (
-              <Button>操作</Button>
-          )
-        }
-    },
-  ];
-  
-  const data = [
-  ];
+import { getArticles } from '../../requests'
 export default class Article extends Component {
+  constructor(){
+    super()
+    this.state={
+      offset: 0,
+      limited: 10,
+      dataSource : [
+        {
+          key: '1',
+          name: '胡彦斌',
+          age: 32,
+          address: '西湖区湖底公园1号'
+        },
+        {
+          key: '2',
+          name: '胡彦祖',
+          age: 42,
+          address: '西湖区湖底公园1号'
+        },
+        {
+          key: '2',
+          name: '胡彦',
+          age: 46,
+          address: '西湖1号'
+        },
+      ],
+      columns :[
+        {
+          title: '姓名',
+          dataIndex: 'name',
+          key: 'name',
+        },
+        {
+          title: '年龄',
+          dataIndex: 'age',
+          key: 'age',
+        },
+        {
+          title: '住址',
+          dataIndex: 'address',
+          key: 'address',
+        },
+        {
+            title: '操作',
+            dataIndex: 'action',
+            key: 'action',
+            render:(text, record, index)=>{
+              return (
+                  <Button>操作</Button>
+              )
+            }
+        },
+      ]
+    }
+  }
+  componentDidMount(){
+    this.getData()
+  }
+  getData(){
+    getArticles(this.state.offset,this.state.limited).then((res)=>{
+      console.log(res.list);
+        // this.setState({
+        //   dataSource:res.list,
+        //   total:res.total
+        // })
+    })
+  }
     toExcel(){
         alert("导出excel")
     }
     render() {
         return (
         <Card title="文章列表" extra={<Button onClick={this.toExcel}>导出excel</Button>} style={{ height:'100%' }}>
-            <Table columns={columns} dataSource={dataSource}
+            <Table columns={this.state.columns} dataSource={this.state.dataSource}
             // 可在Table中使用pagination配置分页器
             pagination={{pageSize:2,total:100}} />
         </Card>
