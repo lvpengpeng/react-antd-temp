31d7c5ba702b016620909e642bf89f4207b3be55
lvpeng <731010527@qq.com>
2020-03-03 18:12:28 +0800

    'feat:仪表盘页面'

--- src/views/Dashboard/index.js
+++ src/views/Dashboard/index.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
-
+import { Card ,Row, Col  } from 'antd'
+import './index.less'
 export default class Dashboard extends Component {
     render() {
         return (
-            <div>
-                Dashboard
-            </div>
+            <>
+                <Card 
+                  title="仪表板"
+                  bordered={false}
+                >
+                    <Row type="flex" justify="start" gutter={16}>
+                        <Col span={6}>
+                            <div className="lp-gutter-box" style={{backgroundColor: '#29B6F6'}}>col-6</div>
+                        </Col>
+                        <Col span={6}>
+                            <div className="lp-gutter-box" style={{backgroundColor: '#AB47BC'}}>col-6</div>
+                        </Col>
+                        <Col span={6}>
+                            <div className="lp-gutter-box" style={{backgroundColor: '#FF7043'}}>col-6</div>
+                        </Col>
+                        <Col span={6}>
+                            <div className="lp-gutter-box" style={{backgroundColor: '#43A047'}}>col-6</div>
+                        </Col>
+                    </Row>
+                </Card>
+            </>
         )
     }
 }
